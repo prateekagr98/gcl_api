a094ad2f92b86e6a345b301bb7c7b0cfe135395b
Prateek Agarwal <prateekagr98@gmail.com>
2018-04-22 11:51:44 +0530

    Avoid duplicates

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,24 @@ var router = express.Router();
 var SubscriptionModel = require('../models/subscription');
 
 router.post('/subscription', function(req, res, next) {
-  SubscriptionModel.create({}, function(err, subscription) {
-  	res.status(201).json({ id: subscription._id });
-  });
+
+	SubscriptionModel.findOne({email: req.body.email}, function(err, existing_subscription){
+		if(err) {
+			res.status(500).json({message: 'Something went wrong'});
+		}
+
+		if(!existing_subscription) {
+			SubscriptionModel.create(req.body, function(err, subscription) {
+		  	if(err) {
+					res.status(500).json({message: 'Something went wrong'});
+				}
+
+		  	res.status(201).json({ id: subscription._id });
+		  });
+		} else {
+			res.status(200).json({id: existing_subscription._id});
+		}
+	});
 });
 
 module.exports = router;
